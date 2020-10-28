eedb2f95c00152b8659f2f89ae22515b05d122ca
kong5000 <keith.ong5000@gmail.com>
2020-10-28 09:13:32 -0700

    Handle localtunnel errors by attempting to reconnect

--- index.js
+++ index.js
@@ -19,14 +19,32 @@ app.get("/", async (req, res) => {
     })
     console.log(e.message)
   }
+});
 
+app.get("/reset", async (req, res) => {
+  try{
+    const data = await axios.get(ARDUINO_STATIC_IP + '/reset', {timeout: 6000})
+    console.log(data.data)
+    res.send(data.data)
+  }catch(e){
+    res.status(400).send({
+      message: 'Reset command failed, Arduino offline?'
+    })
+    console.log(e.message)
+  }
 });
 
 app.listen(3000, async () => {
   console.log("Server listening on port 3000!")
-  const tunnel = await localtunnel({ port: 3000, subdomain: process.env.TUNNEL_SUBDOMAIN });
+  let tunnel = await localtunnel({ port: 3000, subdomain: process.env.TUNNEL_SUBDOMAIN });
   console.log(tunnel.url)
   tunnel.on('close', () => {
     // tunnels are closed
   });
+
+  tunnel.on('error', () => {
+    //attempt to reconnect
+    tunnel = await localtunnel({ port: 3000, subdomain: process.env.TUNNEL_SUBDOMAIN });
+  });
+
 });
